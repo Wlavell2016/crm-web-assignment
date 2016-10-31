9fc54f845c7ec24a06e220d58932844ba1d1f8b6
wlavell <william.lavell@gmail.com>
2016-10-31 13:20:59 -0500

    monday work

--- contact.rb
+++ contact.rb
@@ -1,148 +1,23 @@
-require 'pry'
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
 
-class Contact
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
-	 attr_accessor :first_name, :last_name, :email, :note
-	 attr_reader :id
-	 @@contacts = []
-	 @@id = 1
+class Contact < ActiveRecord::Base
 
-  # This method should initialize the contact's attributes
-  def initialize(first_name , last_name, email, note="")
-	  @first_name = first_name
-	  @last_name = last_name
-	  @email = email
-	  @note = note
-	  @id = @@id
-	  @@id += 1
-  end
-
-  # This method should call the initializer,
-  # store the newly created contact, and then return it
-  def self.create(first_name, last_name, email, note)
-	  new_contact = Contact.new(first_name, last_name, email, note)
-	  @@contacts << new_contact
-	  return new_contact
-  end
-
-  # This method should return all of the existing contacts
-  def self.all
-		return @@contacts
-  end
-
-  # This method should accept an id as an argument
-  # and return the contact who has that id
-  def self.find(id)
-	  @@contacts.each do |contact|
-		  if contact.id == id
-			  return contact
-		  end
-	  end
-  end
-
-  def self.find_first_name(first_name)
-	@@contacts.each do |contact|
-		if contact.first_name.downcase == first_name.downcase
-			return contact
-		end
-	end
-  end
-
-  def self.find_last_name(last_name)
-	@@contacts.each do |contact|
-		if contact.last_name == last_name
-			return contact
-		end
-	end
-	return false
-  end
-
-  def self.find_email(email)
-	@@contacts.each do |contact|
-		if contact.email == email
-			return contact
-		end
-	end
-  end
-
-
-  # This method should allow you to specify
-  # 1. which of the contact's attributes you want to update
-  # 2. the new value for that attribute
-  # and then make the appropriate change to the contact
-  def update(attribute, value)
-	  attri_update = attribute
-	  attri_value = value
-	  if attri_update == "first_name"
-		  self.first_name = attri_value
-	  elsif
-		  attri_update == "last_name"
-		  self.last_name = attri_value
-	  elsif
-		  attri_update == "email"
-		  self.email = attri_value
-	  elsif
-		  attri_update == "note"
-		  self.note = attri_value
-	  else
-		  puts "wtf"
-		  return attri_value
-	  end
-  end
-
-  # This method should work similarly to the find method above
-  # but it should allow you to search for a contact using attributes other than id
-  # by specifying both the name of the attribute and the value
-  # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by(attribute, value)
-	  attri_name = attribute
-	  attri_value = value
-	case attri_name
-		  when "first_name"
-			  find_first_name(attri_value)
-		  when  "last_name"
-			  find_last_name(attri_value)
-		  when  "email"
-			  find_email(attri_value)
-		  else
-			"finish"
-		  end
-	 end
-
-
-
-  # This method should delete all of the contacts
-  def self.delete_all
-	  @@contacts = []
-  end
+	field :first_name, as: :string
+	field :last_name,  as: :string
+	field :email,	   as: :string
+	field :note, 	   as: :text
 
   def full_name
 	  return "#{first_name} #{last_name}"
   end
-
-=begin
- def findindex
-	  @@contacts.each_with_index do |contact, index|
-		  if @id == contact.id
-			  return index
-		  end
-	  end
-=end
-  # This method should delete the contact
-  # HINT: Check the Array class docs for built-in methods that might be useful here
-  def delete
-	  @@contacts.delete(self)
-  end
-
-  # Feel free to add other methods here, if you need them.
-
 end
 
-William = Contact.create("william","lavell","william.lavell@gmail.com","is amazing")
-Sam = Contact.create("Sam","smith","Sam.lavell@gmail.com","is amazing")
+Contact.auto_upgrade!
 
-
-
-
-
-#   ruby test/contact_test.rb
+after do
+	ActiveRecord::Base.connection.close
+end
--- crm.rb
+++ crm.rb
@@ -1,31 +1,73 @@
 require 'sinatra'
 require_relative 'contact'
 
-
-# Fake data
-
-
 get '/' do
 	@crm_app_name = "Bitmaker's CRM"
-	@server_time = Time.now
    erb :index
 end
 
 get '/contacts' do
+	@crm_app_name = "Bitmaker's CRM"
  #@contacts = params[:contacts].upcase
  erb :contacts
 end
 
 get '/contacts/new' do
+	@crm_app_name = "Bitmaker's CRM"
  #@contacts = params[:contacts].upcase
  erb :new_contact
 end
 
 post '/contacts' do
-	puts params
+@crm_app_name = "Bitmaker's CRM"
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
 end
+
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+	erb :show_contact
+  else
+	raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+	erb :edit_contact
+  else
+	raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+	@contact.first_name = params[:first_name]
+	@contact.last_name = params[:last_name]
+	@contact.email = params[:email]
+	@contact.note = params[:note]
+
+	redirect to('/contacts')
+  else
+	raise Sinatra::NotFound
+  end
+end
+
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+	@contact.delete
+	redirect to('/contacts')
+  else
+	raise Sinatra::NotFound
+  end
+end
+
+
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
